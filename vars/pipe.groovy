847e0e966a836054e838883c16b10dec8f344cbf
Arijit Nandi <44856890+ArijitNandi101@users.noreply.github.com>
2019-09-30 11:51:21 +0530

    Update pipe.groovy

--- vars/pipe.groovy
+++ vars/pipe.groovy
@@ -3,8 +3,16 @@ def call(def someSr="none"){
     agent any
     stages{
       stage("Boop boop"){
+        input {
+                message "Should we continue?"
+                ok "Yes, we should."
+                submitter "alice,bob"
+                parameters {
+                    string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+                }
+            }
         steps{
-          echo "$someSr"
+          echo "$someSr $params.PERSON"
         }
       }
     }
